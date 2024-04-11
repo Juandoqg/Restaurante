821da7a5e118fbbec3f94008240f911ff14ba23c
jm0sp1923 <jm0sp1923@gmail.com>
2024-04-11 12:32:37 -0500

    arreglo metodo ver mesas

--- myapp/static/js/listMesas.js
+++ myapp/static/js/listMesas.js
@@ -6,10 +6,10 @@ const listMesas = async () => {
     try {
         const response = await fetch('http://127.0.0.1:8000/listMesas/');
         const data = await response.json();
-        
+
         // Selecciona el contenedor donde se mostrarán las tarjetas de mesa
         const mesasContainer = document.getElementById('mesasContainer');
-        
+
         // Recorre los datos de las mesas y crea una tarjeta para cada una
         data.mesa.forEach(mesa => {
             // Crea un elemento de div para la tarjeta de la mesa
@@ -18,14 +18,17 @@ const listMesas = async () => {
 
             // Crea el contenido HTML de la tarjeta
             cardDiv.innerHTML = `
-                <div class="card mt-2">
-                    <div class="card-body">
-                        <h5 class="card-title">Mesa ${mesa.idMesa}</h5>
-                        <a href="#" class="btn btn-primary">Reservar</a>
-                    </div>
+            <div class="card mt-2">
+            <div class="card-body d-flex flex-column">
+                <h5 class="card-title text-center mb-4">Mesa ${mesa.idMesa}</h5> <!-- Centra el título verticalmente y agrega un margen inferior -->
+                <div class="d-flex justify-content-between mb-4"> <!-- Crea un contenedor flex para los botones con espacio entre ellos -->
+                    <a href="#" class="btn btn-primary">Realizar pedido</a>
+                    <a href="#" class="btn btn-primary">Ver pedido</a>
                 </div>
-            `;
-            
+            </div>
+        </div>
+        `;
+
             // Agrega la tarjeta al contenedor de las mesas
             mesasContainer.appendChild(cardDiv);
         });
