14273ed5d89ca32709a035d4de66eb673cc4c2f8
Juandoqg <juandoqg@gmail.com>
2024-04-11 16:58:14 -0500

    Arreglo de la ruta de imagen

--- myapp/static/js/listMesas.js
+++ myapp/static/js/listMesas.js
@@ -21,6 +21,7 @@ const listMesas = async () => {
             <div class="card mt-2">
             <div class="card-body d-flex flex-column">
                 <h5 class="card-title text-center mb-4">Mesa ${mesa.idMesa}</h5> <!-- Centra el título verticalmente y agrega un margen inferior -->
+                <img src="/static/img/mesa.jpg" alt="Imagen de la mesa" class="card-img-top">
                 <div class="d-flex justify-content-between mb-4"> <!-- Crea un contenedor flex para los botones con espacio entre ellos -->
                     <a href="#" class="btn btn-primary">Realizar pedido</a>
                     <a href="#" class="btn btn-primary">Ver pedido</a>
