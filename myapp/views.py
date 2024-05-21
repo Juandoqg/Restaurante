1309c50eeb35484a256f79f2ed9d4cf1a6f3e5d8
jm0sp1923 <jm0sp1923@gmail.com>
2024-05-21 18:26:11 -0500

    arreglo url y view editar usuario

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
+from django.http import HttpResponseNotAllowed, HttpResponseServerError
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http.response import JsonResponse
-from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from .models import User
 from .models import Mesa
@@ -9,8 +9,8 @@ from .models import Pedido
 from mysite import settings
 from django.contrib.auth.decorators import login_required
 import os
-from django.core import serializers
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 def signin(request):
@@ -226,16 +226,39 @@ def cambiar_estado_pedido(request, pedido_id):
     pedido.save()
     return redirect('chef') 
 
-@login_required
-def  actualizarDatosUsuario(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    if request.method == 'POST':   
-        user.is_active = request.POST.get('is_active') == 'true'
-        user.email = request.POST.get('email')
-        user.first_name = request.POST.get('first_name')
-        user.last_name = request.POST.get('last_name')
-        user.username = request.POST.get('username')
-        user.save()
+@csrf_exempt
+def actualizarDatosUsuario(request, user_id):
+    if request.method == 'PUT':
+        try:
+            user = get_object_or_404(User, id=user_id)
+            data = json.loads(request.body)
+
+            # Actualiza los campos del usuario
+            user.email = data.get('email', user.email)
+            user.first_name = data.get('first_name', user.first_name)
+            user.last_name = data.get('last_name', user.last_name)
+            user.username = data.get('username', user.username)
+
+            # Guarda el usuario actualizado
+            user.save()
+
+            # Devuelve una respuesta de éxito
+            return JsonResponse({
+                'id': user.id,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'username': user.username
+            })
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return HttpResponseNotAllowed(['PUT'])
+
+
     
    
 @login_required
